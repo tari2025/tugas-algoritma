baca65f6e34cf420a40b92c22790d48379d47486
tari2025 <denilestari272@gmail.com>
2025-03-23 15:47:58 +0700

    memasukkan data array

--- pertemuan ke4/insertionSort.cpp
+++ pertemuan ke4/insertionSort.cpp
@@ -6,11 +6,24 @@ int n;       // membuat variable inputan
 
 void input()
 { // procedure input
-    while
-        true
-        {
-            cout << "Masukkan jumlah data pada Array : "; // membuat inputan jumlah emlement array
-            cin >> n;                                     // memanggil variable inputan n
+    while (true)
+    {
+        cout << "Masukkan jumlah data pada Array : "; // membuat inputan jumlah emlement array
+        cin >> n;                                     // memanggil variable inputan n
 
-            if (n <= 20)
-            { // membuat kondisi n tidak lebih dari 20
+        if (n <= 20)
+        { // membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl;                             // membuat jarak per baris program
+    cout << "======================" << endl; // membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0 i < ; i++) // menggunakan perulangan for untuk menyimpan data pada array
+    {
