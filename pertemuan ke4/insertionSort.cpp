c38dea47c40a14292a4af28b7176aa39b91632bb
tari2025 <denilestari272@gmail.com>
2025-03-23 16:03:38 +0700

    menampilkan ke layar

--- pertemuan ke4/insertionSort.cpp
+++ pertemuan ke4/insertionSort.cpp
@@ -54,3 +54,12 @@ j--; // step 4b
         arr[j + 1] = temp; // step 5
     }
 }
+
+void display()
+{
+    cout << endl; // output baris kosong
+    cout << "===================================" endl;
+    cout << "Total passs = " << n - 1 << endl;             // count element movement
+    cout << "===================================" << endl; // output ke layar
+    cout << "Element Array yang telah tersusun" << endl;   // output ke layar
+    cout << "===================================" << endl; // output ke layar
