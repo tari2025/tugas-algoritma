3981935d44035c465c712b6455c433c203d3fac8
tari2025 <denilestari272@gmail.com>
2025-03-23 15:39:22 +0700

    prosedur input

---
+++ pertemuan ke4/insertionSort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+usingnamespace std;
+
+int arr[20]; // membuat array dengan panjang data 20
+int n;       // membuat variable inputan
+
+void input()
+{ // procedure input
+    while
+        true
+        {
+            cout << "Masukkan jumlah data pada Array : "; // membuat inputan jumlah emlement array
+            cin >> n;                                     // memanggil variable inputan n
+
+            if (n <= 20)
+            { // membuat kondisi n tidak lebih dari 20
