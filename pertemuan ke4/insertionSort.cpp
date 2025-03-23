4ac3e3e30e644cdbc8239443c736c00f33f9b0a9
tari2025 <denilestari272@gmail.com>
2025-03-23 15:59:12 +0700

    membuat variable data temporer

--- pertemuan ke4/insertionSort.cpp
+++ pertemuan ke4/insertionSort.cpp
@@ -40,3 +40,17 @@ void insertionSort()
 
     for (i = 1; i <= n - 1; i++)
     { // step 1
+
+        temp = arr[i]; // step 2
+
+        j = i 1; // step 3
+
+        while (j >= 0 && arr[j] > temp) // step 4
+        {
+arr[j + 1] = arr[j] > temp) // step 4a
+j--; // step 4b
+        }
+
+        arr[j + 1] = temp; // step 5
+    }
+}
