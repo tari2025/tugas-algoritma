73d422553ad21a05d1150ab192a7f7b2388c0437
tari2025 <denilestari272@gmail.com>
2025-03-23 15:53:52 +0700

    prosedur insertionSort

--- pertemuan ke4/insertionSort.cpp
+++ pertemuan ke4/insertionSort.cpp
@@ -32,4 +32,11 @@ void input()
     }
 }
 
-void
+void insertionSort()
+{ // procedure insertionSort
+
+    int temp; // membuat variable data temporer atau penyimpanan sementara
+    int j, i; // membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++)
+    { // step 1
